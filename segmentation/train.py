d96975492e7bcbe67799d3f406b6bf7ae8a6cedb
eminorhan <aeminorhan@gmail.com>
2024-06-26 11:32:59 -0400

    simplify segmentation presets (defaulting transforms to v2)

--- segmentation/train.py
+++ segmentation/train.py
@@ -16,11 +16,9 @@ from torchvision.transforms import functional as F, InterpolationMode
 
 def get_dataset(args, is_train):
     def sbd(*args, **kwargs):
-        kwargs.pop("use_v2")
         return torchvision.datasets.SBDataset(*args, mode="segmentation", **kwargs)
 
     def voc(*args, **kwargs):
-        kwargs.pop("use_v2")
         return torchvision.datasets.VOCSegmentation(*args, **kwargs)
 
     paths = {
@@ -31,13 +29,13 @@ def get_dataset(args, is_train):
     p, ds_fn, num_classes = paths[args.dataset]
 
     image_set = "train" if is_train else "val"
-    ds = ds_fn(p, image_set=image_set, transforms=get_transform(is_train, args), use_v2=args.use_v2)
+    ds = ds_fn(p, image_set=image_set, transforms=get_transform(is_train, args))
     return ds, num_classes
 
 
 def get_transform(is_train, args):
     if is_train:
-        return presets.SegmentationPresetTrain(base_size=520, crop_size=480, backend=args.backend, use_v2=args.use_v2)
+        return presets.SegmentationPresetTrain(base_size=520, crop_size=480)
     elif args.weights and args.test_only:
         weights = torchvision.models.get_weight(args.weights)
         trans = weights.transforms()
@@ -50,7 +48,7 @@ def get_transform(is_train, args):
 
         return preprocessing
     else:
-        return presets.SegmentationPresetEval(base_size=520, backend=args.backend, use_v2=args.use_v2)
+        return presets.SegmentationPresetEval(base_size=520)
 
 
 def criterion(inputs, target):
@@ -77,8 +75,7 @@ def evaluate(model, data_loader, device, num_classes):
             output = output["out"]
 
             confmat.update(target.flatten(), output.argmax(1).flatten())
-            # FIXME need to take into account that the datasets
-            # could have been padded in distributed setup
+            # FIXME need to take into account that the datasets could have been padded in distributed setup
             num_processed_samples += image.shape[0]
 
         confmat.reduce_from_all_processes()
@@ -126,11 +123,6 @@ def train_one_epoch(model, criterion, optimizer, data_loader, lr_scheduler, devi
 
 
 def main(args):
-    if args.backend.lower() != "pil" and not args.use_v2:
-        # TODO: Support tensor backend in V1?
-        raise ValueError("Use --use-v2 if you want to use the tv_tensor or tensor backend.")
-    if args.use_v2 and args.dataset != "coco":
-        raise ValueError("v2 is only support supported for coco dataset for now.")
 
     if args.output_dir:
         utils.mkdir(args.output_dir)
@@ -166,7 +158,11 @@ def main(args):
     )
 
     data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=1, sampler=test_sampler, num_workers=args.workers, collate_fn=utils.collate_fn
+        dataset_test, 
+        batch_size=1, 
+        sampler=test_sampler, 
+        num_workers=args.workers, 
+        collate_fn=utils.collate_fn
     )
 
     model = torchvision.models.get_model(
@@ -197,28 +193,18 @@ def main(args):
     scaler = torch.cuda.amp.GradScaler() if args.amp else None
 
     iters_per_epoch = len(data_loader)
-    main_lr_scheduler = PolynomialLR(
-        optimizer, total_iters=iters_per_epoch * (args.epochs - args.lr_warmup_epochs), power=0.9
-    )
+    main_lr_scheduler = PolynomialLR(optimizer, total_iters=iters_per_epoch * (args.epochs - args.lr_warmup_epochs), power=0.9)
 
     if args.lr_warmup_epochs > 0:
         warmup_iters = iters_per_epoch * args.lr_warmup_epochs
         args.lr_warmup_method = args.lr_warmup_method.lower()
         if args.lr_warmup_method == "linear":
-            warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(
-                optimizer, start_factor=args.lr_warmup_decay, total_iters=warmup_iters
-            )
+            warmup_lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=args.lr_warmup_decay, total_iters=warmup_iters)
         elif args.lr_warmup_method == "constant":
-            warmup_lr_scheduler = torch.optim.lr_scheduler.ConstantLR(
-                optimizer, factor=args.lr_warmup_decay, total_iters=warmup_iters
-            )
+            warmup_lr_scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer, factor=args.lr_warmup_decay, total_iters=warmup_iters)
         else:
-            raise RuntimeError(
-                f"Invalid warmup lr method '{args.lr_warmup_method}'. Only linear and constant are supported."
-            )
-        lr_scheduler = torch.optim.lr_scheduler.SequentialLR(
-            optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[warmup_iters]
-        )
+            raise RuntimeError(f"Invalid warmup lr method '{args.lr_warmup_method}'. Only linear and constant are supported.")
+        lr_scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, schedulers=[warmup_lr_scheduler, main_lr_scheduler], milestones=[warmup_iters])
     else:
         lr_scheduler = main_lr_scheduler
 
@@ -268,59 +254,31 @@ def get_args_parser(add_help=True):
     import argparse
 
     parser = argparse.ArgumentParser(description="PyTorch Segmentation Training", add_help=add_help)
-
     parser.add_argument("--data-path", default="/datasets01/COCO/022719/", type=str, help="dataset path")
     parser.add_argument("--dataset", default="coco", type=str, help="dataset name")
     parser.add_argument("--model", default="fcn_resnet101", type=str, help="model name")
     parser.add_argument("--aux-loss", action="store_true", help="auxiliary loss")
     parser.add_argument("--device", default="cuda", type=str, help="device (Use cuda or cpu Default: cuda)")
-    parser.add_argument(
-        "-b", "--batch-size", default=8, type=int, help="images per gpu, the total batch size is $NGPU x batch_size"
-    )
-    parser.add_argument("--epochs", default=30, type=int, metavar="N", help="number of total epochs to run")
-
-    parser.add_argument(
-        "-j", "--workers", default=16, type=int, metavar="N", help="number of data loading workers (default: 16)"
-    )
+    parser.add_argument("--batch-size", default=8, type=int, help="images per gpu, the total batch size is $NGPU x batch_size")
+    parser.add_argument("--epochs", default=30, type=int, help="number of total epochs to run")
+    parser.add_argument("--workers", default=16, type=int, help="number of data loading workers (default: 16)")
     parser.add_argument("--lr", default=0.01, type=float, help="initial learning rate")
-    parser.add_argument("--momentum", default=0.9, type=float, metavar="M", help="momentum")
-    parser.add_argument(
-        "--wd",
-        "--weight-decay",
-        default=1e-4,
-        type=float,
-        metavar="W",
-        help="weight decay (default: 1e-4)",
-        dest="weight_decay",
-    )
+    parser.add_argument("--momentum", default=0.9, type=float, help="momentum")
+    parser.add_argument("--weight-decay", default=1e-4, type=float, help="weight decay (default: 1e-4)", dest="weight_decay")
     parser.add_argument("--lr-warmup-epochs", default=0, type=int, help="the number of epochs to warmup (default: 0)")
     parser.add_argument("--lr-warmup-method", default="linear", type=str, help="the warmup method (default: linear)")
     parser.add_argument("--lr-warmup-decay", default=0.01, type=float, help="the decay for lr")
     parser.add_argument("--print-freq", default=10, type=int, help="print frequency")
     parser.add_argument("--output-dir", default=".", type=str, help="path to save outputs")
     parser.add_argument("--resume", default="", type=str, help="path of checkpoint")
-    parser.add_argument("--start-epoch", default=0, type=int, metavar="N", help="start epoch")
-    parser.add_argument(
-        "--test-only",
-        dest="test_only",
-        help="Only test the model",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only."
-    )
-    # distributed training parameters
+    parser.add_argument("--start-epoch", default=0, type=int, help="start epoch")
+    parser.add_argument("--test-only", dest="test_only", help="Only test the model", action="store_true")
+    parser.add_argument("--use-deterministic-algorithms", action="store_true", help="Forces the use of deterministic algorithms only.")
     parser.add_argument("--world-size", default=1, type=int, help="number of distributed processes")
     parser.add_argument("--dist-url", default="env://", type=str, help="url used to set up distributed training")
-
     parser.add_argument("--weights", default=None, type=str, help="the weights enum name to load")
     parser.add_argument("--weights-backbone", default=None, type=str, help="the backbone weights enum name to load")
-
-    # Mixed precision training parameters
     parser.add_argument("--amp", action="store_true", help="Use torch.cuda.amp for mixed precision training")
-
-    parser.add_argument("--backend", default="PIL", type=str.lower, help="PIL or tensor - case insensitive")
-    parser.add_argument("--use-v2", action="store_true", help="Use V2 transforms")
     return parser
 
 
